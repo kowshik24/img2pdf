4f6b1e9f5abafe7e27079b8890dca1dcf9b115bf
Kowshik Deb Nath <kowshikcseruet1998@gmail.com>
2024-04-20 06:30:21 +0600

    post added

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def transform_to_white_endpoint():
     return send_file(transformed_pdf_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(port=5110, debug=True)
